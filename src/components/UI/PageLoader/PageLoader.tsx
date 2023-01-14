aa21ea407191b1fb9802293803b0a3a5ef3daa35
Kushnir E <kyshnirxeneea@gmail.com>
2023-01-14 12:29:02 +0200

    refactor: page loader component

--- src/components/UI/PageLoader/PageLoader.tsx
+++ src/components/UI/PageLoader/PageLoader.tsx
@@ -7,15 +7,15 @@ function PageLoader() {
     <div className="page-loader">
       <ContentLoader
         speed={2}
-        width={640}
+        width="100%"
         height={400}
-        viewBox="0 0 640 400"
+        viewBox="0 0 100% 400"
         backgroundColor="#f3f3f3"
         foregroundColor="#ecebeb"
       >
         <rect x="0" y="0" rx="10" ry="10" width="40" height="40" />
         <rect x="50" y="0" rx="10" ry="10" width="100" height="40" />
-        <rect x="0" y="60" rx="20" ry="20" width="640" height="340" />
+        <rect x="0" y="60" rx="20" ry="20" width="100%" height="340" />
       </ContentLoader>
     </div>
   );
