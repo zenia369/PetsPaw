e5d33c35771a46891f2b1339b66d74df40c35d06
Kushnir E <kyshnirxeneea@gmail.com>
2023-10-04 11:52:31 +0300

    refactor: update getUserIdLocal helper

--- src/helpers/getUserIdLocal.ts
+++ src/helpers/getUserIdLocal.ts
@@ -2,7 +2,7 @@ import { getItemLS, setItemLS } from "./localStorage";
 
 export default (): string => {
   try {
-    const uid = getItemLS("user_id") as string;
+    const uid = getItemLS<string>("user_id");
     return uid;
   } catch (error) {
     const newUid = Date.now().toString();
