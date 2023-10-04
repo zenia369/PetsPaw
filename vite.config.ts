ff661db99ea1200a45cf48212373eb524c696241
Kushnir E <kyshnirxeneea@gmail.com>
2023-10-04 13:30:57 +0300

    build: add workbox

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: "auto",
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
+        runtimeCaching: [
+          {
+            urlPattern: /\/assets\//,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "assets",
+              expiration: {
+                maxAgeSeconds: 30 * 24 * 60,
+              },
+            },
+          },
+        ],
+      },
+      devOptions: {
+        enabled: false,
+      },
+    }),
+  ],
+  server: {
+    port: 5173,
+    open: true,
+  },
+});
