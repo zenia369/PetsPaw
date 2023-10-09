92db1f76f360a5287f6f1ab699e73369fb0389bc
Kushnir E <kyshnirxeneea@gmail.com>
2023-10-09 19:21:01 +0300

    test: add tests for breeds service

---
+++ src/services/breeds.service.test.ts
@@ -0,0 +1,47 @@
+import { AxiosError } from "axios";
+import { hasDataItem } from "../test/data/data";
+import * as breedService from "./breeds.service";
+
+test("get breed by id", async () => {
+  const response = await breedService.getBreedById("YZGOUtRQ1");
+
+  expect(hasDataItem(response.id)).toBeTruthy();
+});
+
+test("get breed by wrong id should fail", async () => {
+  const { response } = (await breedService
+    .getBreedById("FAIL")
+    .catch((e) => e)) as AxiosError<{ message: string }>;
+
+  expect(response?.status).toBe(400);
+  expect(response?.data.message).toBe("image item is not found by id:FAIL");
+});
+
+test("get breeds", async () => {
+  const response = await breedService.getBreeds();
+
+  expect(response[0]).toEqual(breedService.MOCK_BREEDS_ITEM);
+  expect(Array.isArray(response)).toBeTruthy();
+});
+
+test("get breeds with params", async () => {
+  const mockBreedsIds = "abys,munc";
+  const response = await breedService.getBreedsWithParams({
+    limit: 5,
+    order: "ASC",
+    breed_ids: mockBreedsIds,
+  });
+
+  const resBreedsIds = Object.keys(
+    response.reduce((acc, { breeds }) => {
+      const { id } = breeds[0];
+
+      return { ...acc, [id]: id };
+    }, {})
+  ).toString();
+
+  expect(response.length).toBe(5);
+  expect(resBreedsIds).toBe(mockBreedsIds);
+  expect(response.at(0)!.breeds.at(0)!.id.startsWith("a")).toBeTruthy();
+  expect(response.at(-1)!.breeds.at(-1)!.id.startsWith("m")).toBeTruthy();
+});
--- src/services/breeds.service.ts
+++ src/services/breeds.service.ts
@@ -1,51 +1,13 @@
 import appApi from "./appApi";
 
 import IBreed from "../models/IBreed";
-import ICategory from "../models/ICategory";
 import { IDropDownItem } from "../models/IDropDownList";
 
-const ALL_CATEGORIES_NAME = "All Cateories";
 export const ALL_BREEDS = "All";
-
-export const getCategories = async (): Promise<IDropDownItem[]> => {
-  const { data } = await appApi.get<IDropDownItem[]>("categories");
-
-  return [
-    { id: "all categories", name: ALL_CATEGORIES_NAME, active: true },
-    ...data.map((el) => ({ ...el, active: false })),
-  ];
-};
-
-function createFetchListItem(id: string | number | undefined, limit: number) {
-  return appApi.get(`images/search?category_ids=${id}&limit=${limit}`);
-}
-
-export const getList = async (
-  categories: IDropDownItem[] | undefined,
-  limit: number
-): Promise<ICategory[]> => {
-  const list: ICategory[][] = [];
-  if (categories) {
-    const activeCategories = categories.find((c) => c.active) as IDropDownItem;
-
-    if (activeCategories.name === ALL_CATEGORIES_NAME) {
-      // eslint-disable-next-line no-plusplus
-      for (let i = 0; i < categories.length; i++) {
-        if (categories[i].id) {
-          const { id } = categories[i];
-          // eslint-disable-next-line no-await-in-loop
-          const { data } = await createFetchListItem(id, limit);
-          list.push(data);
-        }
-      }
-    } else {
-      const { data } = await createFetchListItem(activeCategories.id, limit);
-      list.push(data);
-    }
-
-    return list.reduce((acc, c) => [...acc, ...c], []);
-  }
-  return Promise.resolve([]);
+export const MOCK_BREEDS_ITEM = {
+  name: ALL_BREEDS,
+  active: true,
+  id: crypto.randomUUID(),
 };
 
 export const getBreedById = async (id: string) => {
@@ -55,21 +17,16 @@ export const getBreedById = async (id: string) => {
 
 export const getBreeds = async () => {
   const { data } = await appApi.get<IDropDownItem[]>("breeds");
-
-  return [
-    {
-      name: ALL_BREEDS,
-      active: true,
-      id: Date.now(),
-    },
-    ...data,
-  ];
+  return [MOCK_BREEDS_ITEM, ...data];
 };
 
-export const getListWithParams = async (params: object) => {
+export const getBreedsWithParams = async (params: {
+  limit?: number;
+  order?: string;
+  breed_ids?: string;
+}) => {
   const { data } = await appApi.get<IBreed[]>("images/search", {
     params,
   });
-
   return data;
 };
