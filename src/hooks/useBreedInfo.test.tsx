7f7ae07ca7387025657c11e17487e814a0b4ecb1
Kushnir E <kyshnirxeneea@gmail.com>
2023-10-10 23:12:15 +0300

    test: add test useBreedInfo hook

---
+++ src/hooks/useBreedInfo.test.tsx
@@ -0,0 +1,29 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactNode } from "react";
+import { renderHook, waitFor } from "../test/test-utils";
+import useBreedInfo from "./useBreedInfo";
+
+const queryClient = new QueryClient();
+const wrapper = ({ children }: { children: ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+afterEach(() => {
+  queryClient.clear();
+});
+
+test("render hook should work", async () => {
+  const mockId = "uk0SrrBbQ";
+  const { result } = renderHook(() => useBreedInfo(mockId), { wrapper });
+
+  await waitFor(() => expect(result.current.isLoading).toBeFalsy());
+
+  expect(result.current.data).toEqual({
+    id: mockId,
+    url: expect.any(String),
+    breeds: expect.any(Array),
+    width: expect.any(Number),
+    height: expect.any(Number),
+  });
+  expect(result.current.data?.breeds.length).toBeTruthy();
+});
