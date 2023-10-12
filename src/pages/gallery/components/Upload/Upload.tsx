59b56769c3eb1871d7ce908c95741af3fd26df35
Kushnir E <kyshnirxeneea@gmail.com>
2023-10-12 14:52:49 +0300

    refactor: add testid or aria-label in upload component

--- src/pages/gallery/components/Upload/Upload.tsx
+++ src/pages/gallery/components/Upload/Upload.tsx
@@ -50,23 +50,26 @@ function Upload() {
         <a href="https://thecatapi.com/privacy">upload guidelines</a> or face
         deletion.
       </p>
-      <div
+      <button
         className="upload__img"
         onClick={handleInputFileClick}
         onDragOver={(e) => e.preventDefault()}
         onDrop={handleDragDropFile}
+        type="button"
+        aria-label="uploader"
       >
         <input
           type="file"
           ref={inputFileRef}
           accept="image/*, .jpg, .jpeg, .png"
           onChange={handleChangeInputFile}
+          data-testid="uploader"
         />
         {file ? (
           <img
             className="upload__img__full"
             src={previewUrl}
-            alt="uploaded data"
+            alt="uploaded content"
           />
         ) : (
           <div className="upload__img__none">
@@ -77,7 +80,7 @@ function Upload() {
             </p>
           </div>
         )}
-      </div>
+      </button>
       <div className="upload__controls">
         {file ? (
           <div className="upload__controls__full">
@@ -86,34 +89,35 @@ function Upload() {
               type="button"
               disabled={isLoading}
               onClick={() => mutate(file)}
+              aria-label="upload photo"
             >
               UPLOAD PHOTO
             </button>
           </div>
         ) : (
-          <p className="upload__controls__none">No file selected</p>
+          <p className="upload__controls__none" data-testid="no file selected">
+            No file selected
+          </p>
         )}
       </div>
-      {(isSuccess || isError) && (
-        <div className="upload__status">
-          {isSuccess && (
-            <>
-              <img src={uploadOk} alt="upload ok" />
-              <p className="upload__status__ok">
-                Thanks for the Upload - Cat found!
-              </p>
-            </>
-          )}
-          {isError && (
-            <>
-              <img src={uploadErr} alt="upload err" />
-              <p className="upload__status__err">
-                No Cat found - try a different one.
-              </p>
-            </>
-          )}
-        </div>
-      )}
+      <div className="upload__status">
+        {isSuccess && (
+          <>
+            <img src={uploadOk} alt="upload ok" />
+            <p className="upload__status__ok" data-testid="successful uploaded">
+              Thanks for the Upload - Cat found!
+            </p>
+          </>
+        )}
+        {isError && (
+          <>
+            <img src={uploadErr} alt="upload err" />
+            <p className="upload__status__err" data-testid="fail uploaded">
+              No Cat found - try a different one.
+            </p>
+          </>
+        )}
+      </div>
     </div>
   );
 }
